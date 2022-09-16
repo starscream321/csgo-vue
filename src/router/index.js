af9cda4628744a956857a8785c5f932fe0eca38c
Anton <antonegorov489@gmail.com>
2022-09-17 02:36:42 +0300

    1.2

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import LoginPage from "@/page/LoginPage";
 import MainPage from "@/page/MainPage";
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
     routes
 })
 
